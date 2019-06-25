42b891b8d9cfc588322c9326372460c59e846208
wizardforcel <562826179@qq.com>
2019-06-25 15:06:28 +0800

    2019-06-25 15:06:28

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ module.exports = {
   },
   hooks: {
     'page:before': function(page) {
-      /*
       var _label = 'File Modify: ',
           _format = 'YYYY-MM-DD HH:mm:ss',
           _copy = '由 ApacheCN 团队提供支持'
@@ -20,7 +19,7 @@ module.exports = {
         _copy = _c ? _c + ' all right reserved，' + _copy : _copy;
       }
       var _copy = '<span class="copyright">'+_copy+'</span>'
-      var str = [
+      var credit = [
         '\n\n<footer class="page-footer">',
         '<div>',
         _copy,
@@ -29,9 +28,8 @@ module.exports = {
         '{{file.mtime | date("' + _format + '")}}',
         '</span>',
         '</div>',
-        '</footer>'
+        '</footer>',
       ].join('\n')
-      */
       var gittalk = [
           '<div>',
           '  <link rel="stylesheet" href="https://unpkg.com/gitalk/dist/gitalk.css">',
@@ -66,15 +64,16 @@ module.exports = {
       var footer = [
         '<hr/>',
         '<div align="center">',
-        '<p><a href="http://www.apachecn.org/" target="_blank"><font face="KaiTi" size="6" color="red">我们一直在努力</font></a><p>',
-        '<p><a href="https://github.com/apachecn/pytorch-doc-zh/" target="_blank">apachecn/pytorch-doc-zh</a></p>',
-        '<p><iframe align="middle" src="https://ghbtns.com/github-btn.html?user=apachecn&repo=pytorch-doc-zh&type=watch&count=true&v=2" frameborder="0" scrolling="0" width="100px" height="25px"></iframe>',
-        '<iframe align="middle" src="https://ghbtns.com/github-btn.html?user=apachecn&repo=pytorch-doc-zh&type=star&count=true" frameborder="0" scrolling="0" width="100px" height="25px"></iframe>',
-        '<iframe align="middle" src="https://ghbtns.com/github-btn.html?user=apachecn&repo=pytorch-doc-zh&type=fork&count=true" frameborder="0" scrolling="0" width="100px" height="25px"></iframe>',
-        '<a target="_blank" href="//shang.qq.com/wpa/qunwpa?idkey=bcee938030cc9e1552deb3bd9617bbbf62d3ec1647e4b60d9cd6b6e8f78ddc03"><img border="0" src="data.apachecn.org/img/logo/ApacheCN-group.png" alt="ML | ApacheCN" title="ML | ApacheCN"></a></p>',
+        '    <p><a href="http://www.apachecn.org/" target="_blank"><font face="KaiTi" size="6" color="red">我们一直在努力</font></a><p>',
+        '    <p><a href="https://github.com/apachecn/pytorch-doc-zh/" target="_blank">apachecn/pytorch-doc-zh</a></p>',
+        '    <p><iframe align="middle" src="https://ghbtns.com/github-btn.html?user=apachecn&repo=pytorch-doc-zh&type=watch&count=true&v=2" frameborder="0" scrolling="0" width="100px" height="25px"></iframe>',
+        '    <iframe align="middle" src="https://ghbtns.com/github-btn.html?user=apachecn&repo=pytorch-doc-zh&type=star&count=true" frameborder="0" scrolling="0" width="100px" height="25px"></iframe>',
+        '    <iframe align="middle" src="https://ghbtns.com/github-btn.html?user=apachecn&repo=pytorch-doc-zh&type=fork&count=true" frameborder="0" scrolling="0" width="100px" height="25px"></iframe>',
+        '    <a target="_blank" href="//shang.qq.com/wpa/qunwpa?idkey=bcee938030cc9e1552deb3bd9617bbbf62d3ec1647e4b60d9cd6b6e8f78ddc03"><img border="0" src="data.apachecn.org/img/logo/ApacheCN-group.png" alt="ML | ApacheCN" title="ML | ApacheCN"></a></p>',
         ad,
         '</div>',
         gittalk,
+        credit,
       ].join('\n')
       page.content = page.content + footer;
       return page;
